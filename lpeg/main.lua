8ba23a18fc33ba22c79ca25f501e75daa24c8ad6
Renan <renan.061@gmail.com>
2019-03-10 19:02:45 -0300

    More LPeg fun

--- lpeg/main.lua
+++ lpeg/main.lua
@@ -1,66 +1,89 @@
 local lpeg = require("lpeg")
 local re = require("re")
 
+local tabs = 0 -- hehe
+function table.print(t)
+    if not t then
+        return print(t)
+    end
+    function printtabs()
+        for i = 1, tabs do
+            io.write(" ")
+        end
+    end
+    for k, v in pairs(t) do
+        printtabs()
+        io.write("* ")
+        if type(v) == "table" then
+            io.write(tostring(k) .. " => \n")
+            tabs = tabs + 4
+            table.print(v)
+            tabs = tabs - 4
+        else
+            io.write(tostring(k) .. " => " ..  tostring(v) .. "\n")
+        end
+    end
+end
+
 -- Isso é um ataque, podemos dar match em um ataque.
 -- local input = "Longsword. Melee Weapon Attack: +8 to hit."
 local input = "Longsword. " ..
     "Melee Weapon Attack: +7 to hit, reach 5 ft., one target. " ..
-    "Hit: 8 (1d8 + 4) slashing damage, " ..
-        "or 9 (1d10 + 4) slashing damage " ..
-        "if used with two hands to make a melee attack."
+    "Hit: 8 (1d8 + 4) slashing damage + 7 (2d6) fire damage."
 
--- local input = "abab"
+local definitions = {}
+function define(t)
+    for k, v in pairs(t) do
+        definitions[k] = re.compile(v, definitions)
+    end
+end
 
--- G = lpeg.P {
---     "S";
---     S = "a" * lpeg.V("B")
---       + "b" * lpeg.V("A")
---       + "",
---     A = "a" * lpeg.V("S")
---       + "b" * lpeg.V("A") * lpeg.V("A"),
---     B = "b" * lpeg.V("S")
---       + "a" * lpeg.V("B") * lpeg.V("B"),
--- } * -1
+-- capture functions
+definitions.tonumber = function(s)
+    s = s:gsub("%s*", "")
+    s = s:gsub("'+'", "")
+    return tonumber(s)
+end
 
-local P, B, R, S, V = lpeg.P, lpeg.B, lpeg.R, lpeg.S, lpeg.V
-local C, Ct = lpeg.C, lpeg.Ct
-
--- local uppercase = C(R"AZ" * R"az"^0)
-
--- local G = lpeg.P {
---     "S",
---     S = V"U" * P(" ") * V"S" + V"U",
---     U = uppercase
--- }
-
-local defs = {
-    space = P" "
-    spaces = spaces^0
-    number = R"09"^1 / tonumber,
-    modifier = (P"+" + "-") * R"09"^1 / tonumber
+-- patterns
+define {
+    S = "%s*",
+    number = "[1-9][0-9]*",
+    die = "'d' ([23468] / '10' / '12' / '20')"
 }
 
-defs.roll = re.compile([[
-    roll <- n 'd' die %spaces ''
-    n    <- [1-9][0-9]*
-    die  <- [468] / '10' / '12' / '20'
-]], defs)
+define { modifier = "('+' / '-') %S %number" }
 
-local G = [[
-    action   <- name types tohit distance targets hit {.*}
+define { roll = "{[1-9][0-9]*} {%die} %S %modifier -> tonumber" }
+    
+define { type = " 'bludgeoning' / 'piercing' / 'slashing' "}
+
+define { damage = "{%number} %S '(' {| %roll |} ')' %S {%type} %S 'damage'" }
+
+-- define { damage_list = [[
+--     damage_list <- damage_list %S '+' %S %damage
+--                  / %damage
+-- ]] }
+
+define { action = [[
+    action   <- {| name types tohit distance targets hit {.*} |}
     name     <- {(!'.'.)+} '. '
-    types    <- {'Melee' / 'Ranged'} ' ' {'Weapon' / 'Spell'} ' Attack: '
-    tohit    <- %modifier ' to hit, '
-    distance <- {'range' / 'reach'} ' ' %number ' ft., '
+    types    <- {| {'Melee' / 'Ranged'} %S {'Weapon' / 'Spell'} ' Attack: ' |}
+    tohit    <- %modifier -> tonumber ' to hit, '
+    distance <- {| {'range' / 'reach'} %S {%number} ' ft., ' |}
     targets  <- {[a-z]+}' target''s'? '. '
-    hit      <- 'Hit: ' %number '(' %roll ')'
-]]
+    hit      <- 'Hit: ' %S {| %damage |}
+]]}
 
-G = Ct(re.compile(G, defs)):match(input)
+lpeg.locale(lpeg)   -- adds locale entries into 'lpeg' table
 
-print(G)
+-- local space = lpeg.space^0
+-- local name = lpeg.C(lpeg.alpha^1) * space
+-- local sep = lpeg.S(",;") * space
+-- local pair = lpeg.Cg(name * "=" * space * name) * sep^-1
+-- local list = lpeg.Cf(lpeg.Ct("") * pair^0, rawset)
+-- t = list:match("a=b, c = hi; next = pi")  --> { a = "b", c = "hi", next = "pi" }
 
-if not G then return nil end
-for k, v in pairs(G) do
-    print(k, v)
-end
+local ast = definitions.action:match(input)
+
+table.print(ast)
